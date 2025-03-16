e351b13da6c261d84456af7e5e993426e02cafaa
Mauricio Ribeiro <mauricio91araujo@gmail.com>
2025-03-16 16:13:26 -0300

    feat: general apis

--- src/DeveloperStore/SalesApi.Application/DTO/Request/ProductDto.cs
+++ src/DeveloperStore/SalesApi.Application/DTO/Request/ProductDto.cs
@@ -1,8 +1,13 @@
-﻿namespace SalesApi.Application.DTO.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesApi.Application.DTO.Request
 {
     public class ProductDto
     {
+        [Required(ErrorMessage = "The title is required.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "The price is required.")]
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
--- src/DeveloperStore/SalesApi.Application/DTO/Request/SaleItemDto.cs
+++ src/DeveloperStore/SalesApi.Application/DTO/Request/SaleItemDto.cs
@@ -7,14 +7,44 @@ using System.Text;
 using System.Threading.Tasks;
 using SalesApi.Domain.Services;
 using System.Text.Json.Serialization;
+using System.Diagnostics;
 
 namespace SalesApi.Application.DTO.Request
 {
-    public class SaleItemDto
+    public class SaleItemDto : IValidatableObject
     {
+        [Required(ErrorMessage = "The product id is required!")]
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
+
         public decimal UnitPrice { get; set; }
         public decimal TotalPrice { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (UnitPrice == 0)
+            {
+                yield return new ValidationResult(
+                    $"The product {ProductId} with zero price",
+                    new[] { nameof(UnitPrice) }
+                );
+            }
+
+            if (TotalPrice == 0)
+            {
+                yield return new ValidationResult(
+                    $"The product {ProductId} with zero total price",
+                    new[] { nameof(TotalPrice) }
+                );
+            }
+
+            if (Quantity == 0)
+            {
+                yield return new ValidationResult(
+                    $"The product {ProductId} with zero quantity",
+                    new[] { nameof(Quantity) }
+                );
+            }
+        }
     }
 }
