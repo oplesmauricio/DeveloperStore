dc1c1872a1ef756165ec9a18d309e92fc932bb3e
Mauricio Ribeiro <mauricio91araujo@gmail.com>
2025-03-18 12:08:50 -0300

    feat: migrations

---
+++ src/SalesApi.Infrastructure/Migrations/20250318141552_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SalesApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    category = table.Column<string>(type: "text", nullable: false),
+                    image = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sales",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    saledate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    salenumber = table.Column<string>(type: "text", nullable: false),
+                    customer = table.Column<Guid>(type: "uuid", nullable: false),
+                    store = table.Column<Guid>(type: "uuid", nullable: false),
+                    iscanceled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sales", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "saleitems",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    saleid = table.Column<int>(type: "integer", nullable: false),
+                    productid = table.Column<Guid>(type: "uuid", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    unitprice = table.Column<decimal>(type: "numeric", nullable: false),
+                    discount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_saleitems", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_saleitems_sales_saleid",
+                        column: x => x.saleid,
+                        principalTable: "sales",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_saleitems_saleid",
+                table: "saleitems",
+                column: "saleid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "saleitems");
+
+            migrationBuilder.DropTable(
+                name: "sales");
+        }
+    }
+}
--- src/SalesApi.Infrastructure/Persistence/ApplicationDbContextFactory .cs
+++ src/SalesApi.Infrastructure/Persistence/ApplicationDbContextFactory .cs
@@ -11,36 +11,22 @@ namespace SalesApi.Infrastructure.Persistence
 {
     public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
-        //private readonly IConfiguration _configuration;
-
-        //public ApplicationDbContextFactory(IConfiguration configuration)
-        //{
-        //    _configuration = configuration;
-        //}
-
         public ApplicationDbContext CreateDbContext(string[] args)
         {
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json")
+            //    .Build();
+
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseNpgsql("Host=172.18.0.2;Port=8001;Database=SalesApiDb;Username=postgres;Password=admin;Timeout=60");
+            //var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql("Host=db;Port=5432;Database=SalesApiDb;Username=postgres;Password=admin"); //este host nao eh conhecido ao executar dotnet ef database update
+
+            //ip do db no docker
+            optionsBuilder.UseNpgsql("Host=172.18.0.2;Port=5432;Database=SalesApiDb;Username=postgres;Password=admin;Timeout=60"); //Timeout during connection attempt ao executar dotnet ef database update mesmo aumentando para 60
 
             return new ApplicationDbContext(optionsBuilder.Options);
         }
     }
-
-    //public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    //{
-    //    public ApplicationDbContext CreateDbContext(string[] args)
-    //    {
-    //        var configuration = new ConfigurationBuilder()
-    //            .SetBasePath(Directory.GetCurrentDirectory())
-    //            .AddJsonFile("appsettings.json")
-    //            .Build();
-
-    //        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-    //        var connectionString = configuration.GetConnectionString("DefaultConnection");
-    //        optionsBuilder.UseNpgsql(connectionString);
-
-    //        return new ApplicationDbContext(optionsBuilder.Options);
-    //    }
-    //}
 }
